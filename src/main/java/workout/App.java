c168bacc24891860ba866067ecc58e90396cdb11
Makhi7 <mvmangxola22@gmail.com>
2020-12-09 11:48:58 +0200

    hack app

---
+++ src/main/java/workout/App.java
@@ -0,0 +1,24 @@
+package workout;
+
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        get("/", (request, response) -> "Hello 1");
+
+        post("/greet", (request, response) -> "Hello 2");
+    }
+
+
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}
